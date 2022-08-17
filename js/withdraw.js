0e1d006b3752407abea54c0a5c56d3689ed304f4
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-17 10:48:07 +0000

    finished module 27-4 Money withdraw and reduce balance for withdraw

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,5 +1,6 @@
 /* 
 1. add event handler with the withdraw button
+
 2. get the withdraw amount
 
 3. clear the withdraw input field
@@ -15,5 +16,29 @@
 
 // step-1
 document.getElementById("btn-withdraw").addEventListener("click", function () {
-  console.log("withdraw calculation");
+  // step-2
+  const withdrawField = document.getElementById("withdraw-field");
+  const newWithdrawAmountString = withdrawField.value;
+  const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+  // step-3
+  withdrawField.value = "";
+
+  // step-4
+  const withdrawTotalElement = document.getElementById("withdraw-total");
+  const previousWithdrawTotalString = withdrawTotalElement.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+  // step-5
+  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = newWithdrawTotal;
+
+  // step-6
+  const balanceElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceElement.innerText; 
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  // step-7
+  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+  balanceElement.innerText = newBalanceTotal;
 });
